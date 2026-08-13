5e5b9a8d9bbf6c715f9b3cefe45ab80ce7c5f851
sidpav1980-alt <sid.pav1980@gmail.com>
2026-08-13 19:26:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE='trail-analyzer-online-v048';
+const CACHE='trail-analyzer-online-v043';
 const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
